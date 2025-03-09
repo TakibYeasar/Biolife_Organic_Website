2f8f930dfb0568bb7858255553700b517a6d1ab4
Takib Yeasar <mdtakibyeasar@gmail.com>
2025-03-09 11:33:30 +0600

    updated database url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,11 +123,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
